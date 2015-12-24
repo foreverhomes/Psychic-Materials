cd426e5ccd8ef1fee8565330a0f4fc1663572b47
Jeff DeButte <jeff@inharmonic.com>
2015-12-24 15:01:34 -0500

    Added soundcloud and Font Awesome

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -15,13 +15,6 @@ angular.module('pm', ['x-gif'])
         var xGif = document.querySelector('x-gif'),
           audio = document.querySelector('audio'),
           metadata;
-
-        $http.get('audio/palms_lose.json', function(data) {
-        	console.log(data);
-        	//metadata = JSON.parse(this.response);
-          	//setupAudioSynching(audio, xGif, metadata);
-          	//audio.play();
-        })
           
 
         // window.addEventListener('resize', function () {
@@ -30,40 +23,8 @@ angular.module('pm', ['x-gif'])
         //   });
         // })
 
-  	var setupAudioSynching = function (audio, xGifs, metadata) {
-  	  var synchOffset = -0.1;
-
-  	  audio.addEventListener('playing', function () {
-  	    var beats = metadata.response.track.analysis.beats,
-  	      beatIndex = 0;
-  	    while (beats[0].start > 0) {
-  	      beats.unshift({
-  	        start: beats[0].start - beats[0].duration,
-  	        duration: beats[0].duration,
-  	        confidence: 0
-  	      })
-  	    }
-
-  	    var animationLoop = function () {
-  	      requestAnimationFrame(animationLoop);
-
-  	      if (beats.length > beatIndex) {
-  	        var currentTime = audio.currentTime + synchOffset;
-  	        while (beatIndex < beats.length && currentTime > beats[beatIndex].start) {
-  	          beatIndex++;
-  	        }
-  	        var beat = beats[beatIndex - 1];
-
-  	        var sinceLastBeat = currentTime - beat.start,
-  	          beatFraction = sinceLastBeat / beat.duration;
-  	        [].forEach.call(xGifs, function (xGif) {
-  	          xGif.clock(beatIndex, beat.duration * 1000 / audio.playbackRate, beatFraction);
-  	        })
-  	      }
-  	    }
-  	    animationLoop();
-  	  });
-  	}
+ 
+  	//}
   }).controller('pmCtrl', function ($scope) {
   	console.log('hey bud')
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,13 @@
 
 $('document').ready(function() {
+
+	SC.initialize({
+	  client_id: '1f33d1f52f43a1cf07a274370125f371'
+	});
+	
+	var audioPlayer;
+	var playButton = $('.play-button');
+
 	$(document).foundation();
 
 	$('.track-data').on('click', function() {
@@ -9,7 +17,45 @@ $('document').ready(function() {
 		var el = parent.find('x-gif')[0];
 		el.hasAttribute('stopped') ? el.removeAttribute('stopped') : el.setAttribute('stopped', '');
 
-		var audio = parent.find('audio')[0];
-		el.hasAttribute('stopped') ? audio.pause() : audio.play();
+		//el.hasAttribute('stopped') ? audio.pause() : audio.play();
+
+		SC.stream('/tracks/204661121').then(function(player){
+		  audioPlayer = player;
+		  audioPlayer.play();
+		  playButton.removeClass('fa-play').addClass('fa-pause');
+		});
 	});
+
+	playButton.on('click', function() {
+		if(typeof audioPlayer === "undefined") {
+			console.log('start playing track 1');
+		} else {
+			if($(this).hasClass('fa-play')) {
+				audioPlayer.play();
+				$(this).removeClass('fa-play').addClass('fa-pause');
+			} else {
+				audioPlayer.pause();
+				$(this).removeClass('fa-pause').addClass('fa-play');
+			}
+		}
+	});
+
+	var audio = $('audio')[0];
+
+	var $audio = $('audio');
+	$audio.on('playing', function() {
+		console.log('playing');
+	});
+
+	$audio.on('ended', function() {
+		$('.current').find('.track-data').removeClass('playing');
+		var el = $('.current').find('x-gif')[0];
+		el.setAttribute('stopped', '');
+	});
+
+	$audio.on('timeupdate', function() {
+		console.log(Math.round(audio.currentTime/audio.duration * 100));
+	});
+
+	audio.currentTime = 220;
 });
